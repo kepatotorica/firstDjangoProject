b079130fd9e940a01898ab471859a6efb5ff0758
kepatoto <kepatoto@gmail.com>
2018-06-19 18:37:00 -0400

    have database changes working for updating songs

--- music/urls.py
+++ music/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     #/music/albumID
     re_path(r'^(?P<album_id>[0-9]+)/$', views.view_id, name='view_id'),
+    # /music/albumID/favorite/
+    re_path(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
-from .models import Album, Song
 from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -7,7 +7,23 @@ def index(request):
     return render(request, 'music/index.html',context)
 
 def view_id(request, album_id):
-
     album = get_object_or_404(Album, pk=album_id)
-    context = {'album': album,}
-    return render(request, 'music/details.html',context)
+    context = {'album': album}
+    return render(request, 'music/details.html', context)
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {
+            'album': album,
+            'error_message': "this isn't a valid song",
+        })
+    else:
+        if selected_song.is_favorite == False:
+            selected_song.is_favorite = True
+        else:
+            selected_song.is_favorite = False
+        selected_song.save()
+        return render(request, 'music/details.html', {'album': album})
