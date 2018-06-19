19450882324219513efc37611adf22ba678414cb
kepatoto <kepatoto@gmail.com>
2018-06-19 11:17:58 -0400

    first commit

---
+++ music/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    #/music/
+    re_path(r'^$', views.index, name='index'),
+    #/music/albumID
+    re_path(r'^(?P<album_id>[0-9]+)/$', views.view_id, name='view_id'),
+    #/music/album
+    re_path('albums',views.view_albums, name='view_albums')
+]
