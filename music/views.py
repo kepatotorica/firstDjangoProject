b132cbaa27bb4283fddfff0acfaa5c8cc229f2ea
kepatoto <kepatoto@gmail.com>
2018-06-19 11:39:36 -0400

    added an html template

--- music/views.py
+++ music/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album
+from django.template import loader
 # Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/'+ str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 def view_id(request, album_id):
     return HttpResponse("<h2>Album id: " + str(album_id) + "</h2>")
